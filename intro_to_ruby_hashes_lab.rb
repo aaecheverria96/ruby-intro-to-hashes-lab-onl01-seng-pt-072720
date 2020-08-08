9595d838332666bf3b99d2a356caba4a2552165e
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-08 23:13:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,7 @@ def monopoly_with_second_tier
 		:pieces => 4
 #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	},
-	:names => {
-
-	}
+	:names => {}
 }
 
 end
